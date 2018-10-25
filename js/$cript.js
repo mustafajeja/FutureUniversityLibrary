d1657e43de4901473c1f249c33c3cc1f6c2ccbb4
MufasaAdam <mustafajeja@gmail.com>
2018-10-25 11:56:38 +0200

    Feature/Closing hours CountDown

--- js/$cript.js
+++ js/$cript.js
@@ -27,3 +27,58 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('.tooltipped').tooltip();
 });
+
+
+
+// CountDown Script 
+(function ($) {
+    var date = new Date(),
+        month = date.getMonth();
+    day = date.getDate(),
+        weekDay = date.getDay(),
+        hours = {
+            start: new Date(date.getFullYear(), month, day),
+            end: new Date(date.getFullYear(), month, day)
+        };
+
+    // var weekDay = [0 = fri, 1 = sat, 2 = sun, 3 = mon, 4 = tues, 5 = wed, 6 = thur]
+
+    // If it's Friday 
+    if (weekDay == 5) {
+
+        $('.countdown').hide(),
+            document.getElementById('head').innerHTML = "The Library is closed";
+            
+    } else {
+
+        // Start at 9am, end at 6pm
+        hours.start.setHours(6);
+        hours.end.setHours(16);
+    }
+
+    function countDown() {
+        var date = new Date(),
+            countHours = ('0' + (hours.end.getHours() - date.getHours())).substr(-2),
+            countMinutes = ('0' + (59 - date.getMinutes())).substr(-2),
+            countSeconds = ('0' + (59 - date.getSeconds())).substr(-2);
+
+        // If it's currently not within the hours, don't show the countdown
+        if (date.getHours() < hours.start.getHours() || date.getHours() > hours.end.getHours()) {
+            $('.countdown').hide(),
+                document.getElementById('head').innerHTML = "The Library is closed";
+        } else if ($('.countdown').not(':visible')) {
+            $('.countdown').show();
+        }
+
+        $('.countdown #hours').text(countHours);
+        $('.countdown #minutes').text(countMinutes);
+        $('.countdown #seconds').html(countSeconds);
+
+    }
+
+    $(function () {
+        setInterval(function () {
+            countDown();
+        }, 1000);
+    });
+})(jQuery);
